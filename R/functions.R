77cfc35eb7a79b14e54f30e80466fbc0d36fdb19
willschulz <wm.s.schulz@gmail.com>
2023-04-21 14:34:09 -0400

    accessing exported functions

--- R/functions.R
+++ R/functions.R
@@ -28,7 +28,7 @@ cat_function <- function(love=TRUE){
 
 parseChats <- function(file) {
   raw_chat_data <- readr::read_lines(file)
-  tops <- which(str_detect(raw_chat_data, pattern = "Room Id:"))
+  tops <- which(stringr::str_detect(raw_chat_data, pattern = "Room Id:"))
   bottoms <- which(raw_chat_data=="----------------------------------")
   chats_list <- list()
   for (i in 1:length(tops)){
@@ -37,7 +37,7 @@ parseChats <- function(file) {
     #room id
     thischatlist$room_id <- gsub(" ", "", x = str_split(thischat[1], ",", simplify = T)[2], fixed = T)
     #started at
-    thischatlist$started_at <- lubridate::parse_date_time(x = str_replace_all(str_remove_all(str_remove(thischat[2], pattern = "Started At:, "), pattern = ","), pattern = "/", replacement = "-"), orders = "mdYHMS", tz = "America/New_York")
+    thischatlist$started_at <- lubridate::parse_date_time(x = stringr::str_replace_all(stringr::str_remove_all(stringr::str_remove(thischat[2], pattern = "Started At:, "), pattern = ","), pattern = "/", replacement = "-"), orders = "mdYHMS", tz = "America/New_York")
     #participant data
     thischatlist$participants <- readr::read_delim(I(thischat[(which(thischat == "Participants") + 1):(which(thischat == "Messages") - 2)]), delim = ",", show_col_types = FALSE)
     #messages
