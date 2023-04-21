316f814a5317750723c9b5fb6b555c302553afae
willschulz <wm.s.schulz@gmail.com>
2023-04-21 14:37:43 -0400

    delete cat function

--- R/functions.R
+++ R/functions.R
@@ -1,22 +1,3 @@
-#' A Cat Function
-#'
-#' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
-#' @export
-#' @examples
-#' cat_function()
-
-cat_function <- function(love=TRUE){
-  if(love==TRUE){
-    print("I love cats!")
-  }
-  else {
-    print("I am not a cool person.")
-  }
-}
-
-
 #' Parse Chats
 #'
 #' This function parses a ReChat csv export into an R list.
